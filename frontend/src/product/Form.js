3b0f3141c753907933a71145b4f0f5a73f792bdf
Sebastian Springer <info@sebastian-springer.com>
2020-11-20 16:41:36 +0100

    Aufgabe 8 Lösung

--- frontend/src/product/Form.js
+++ frontend/src/product/Form.js
@@ -1,25 +1,68 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Form() {
+  const category = 2;
   const initialProduct = {
     title: '',
     price: '',
+    category,
+    description: '',
   };
 
   const [product, setProduct] = useState(initialProduct);
 
-  const [value, setValue] = useState('');
+  function handleChange(e) {
+    const value = e.currentTarget.value;
+    const name = e.currentTarget.name;
+    setProduct((prevProduct) => ({ ...prevProduct, [name]: value }));
+  }
+
+  const history = useHistory();
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await fetch('http://localhost:3001/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    });
+    if (response.status === 201) {
+      history.push('/product/2');
+    }
+  }
 
   return (
-    <div>
-      Value:
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => {
-          setValue(e.currentTarget.value);
-        }}
-      />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <label htmlFor="title">Title: </label>
+        <input
+          type="text"
+          name="title"
+          value={product.title}
+          onChange={handleChange}
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="price">Price: </label>
+        <input
+          type="text"
+          name="price"
+          value={product.price}
+          onChange={handleChange}
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="description">Description:</label>
+        <input
+          type="text"
+          name="description"
+          value={product.description}
+          onChange={handleChange}
+        />
+      </fieldset>
+      <button type="submit">speichern</button>
+    </form>
   );
 }
